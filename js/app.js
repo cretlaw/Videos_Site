252d76a771d4ea94eeff6d8c425c6bb1e7055c95
winterMute84 <wcarbajal84@gmail.com>
2018-08-05 01:29:06 -0700

    implemented show more feature

--- js/app.js
+++ js/app.js
@@ -1,28 +1,47 @@
+let videoApp = new Vue({
+
+    el: '#videoApp',
+
+    data: {
+        showRemainingContent: false,
+        hideBtn: false
+    },
+
+    methods: {
+        showRemaingVideos: function () {
+            this.showRemainingContent = true
+            this.hideBtn = true
+        }
+    }
+
+})
+
+
 let footerApp = new Vue({
     el: '#footerApp',
 
-    data:{
+    data: {
         // you can change the phone and email strings as desired
-        phone:'(123) 456-7891',
-        email:'someemail@email.com',
-        phoneClick:false,
-        emailClick: false 
+        phone: '(123) 456-7891',
+        email: 'someemail@email.com',
+        phoneClick: false,
+        emailClick: false
     },
 
-    methods:{
-        phonePopulate: function(){
+    methods: {
+        phonePopulate: function () {
 
             this.phoneClick = true
-    
+
         },
-        emailPopulate: function(){
-            this.emailClick =true
+        emailPopulate: function () {
+            this.emailClick = true
         },
-        toggleButtons: function(){
+        toggleButtons: function () {
             this.phoneClick = false
             this.emailClick = false
         }
-        
+
     }
 
 })
